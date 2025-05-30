473cd0c460c1a4d3ffcbaf275987cc256530f5ec
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:40:00 -0300

    Create navbar.php

---
+++ app/views/shared/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar is-link" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <strong>SimpleVisitadores</strong>
+    </a>
+  </div>
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/">Dashboard</a>
+      <a class="navbar-item" href="/profissionais">Profissionais</a>
+      <a class="navbar-item" href="/representantes">Representantes</a>
+      <a class="navbar-item" href="/auditoria">Auditoria</a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="button is-light" href="/logout">Sair</a>
+      </div>
+    </div>
+  </div>
+</nav>
