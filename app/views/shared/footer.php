5fa4544c9a43ab8ab750a70ed4886f3633310fff
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:42:11 -0300

    Create footer.php

---
+++ app/views/shared/footer.php
@@ -0,0 +1,9 @@
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>SimpleVisitadores</strong> &copy; <?= date('Y') ?>
+                </p>
+            </div>
+        </footer>
+    </body>
+</html>
