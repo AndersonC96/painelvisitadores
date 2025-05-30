1628d60f472bc8f7e39eb0b89cc3146e4835a652
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 15:08:36 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,18 @@
 <?php
     require_once dirname(__DIR__) . '/vendor/autoload.php';
-    use App\Core\Router;
-    // Carrega variáveis
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
-    // Start session (controle de login)
+    use App\Core\Router;
     session_start();
     $router = new Router();
-    // Rotas de autenticação
     $router->get('/login', 'AuthController@loginForm');
     $router->post('/login', 'AuthController@login');
     $router->get('/logout', 'AuthController@logout');
-    // Demais rotas
     $router->get('/', 'DashboardController@index');
     // ---- Proteção das rotas internas ----
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = '/' . trim($uri, '/'); // Deixa no formato '/rota'
     $rotasPublicas = ['/login', '/logout'];
-    // Se não está logado e não está numa rota pública, redireciona para /login
     if (
         !isset($_SESSION['usuario_id']) &&
         !in_array($uri, $rotasPublicas)
@@ -25,6 +20,4 @@
         header('Location: /login');
         exit;
     }
-    // Dispara o roteador normalmente
     $router->dispatch();
-?>
