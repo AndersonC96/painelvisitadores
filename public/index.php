5201c662fd0caba46e7e3338e5b7a83928808ae0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:54:32 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,5 +4,11 @@
     // Start session (controle de login)
     session_start();
     $router = new Router();
+    // Rotas de autenticação
+    $router->get('/login', 'AuthController@loginForm');
+    $router->post('/login', 'AuthController@login');
+    $router->get('/logout', 'AuthController@logout');
+    // Demais rotas
+    $router->get('/', 'DashboardController@index');
     $router->dispatch();
 ?>
