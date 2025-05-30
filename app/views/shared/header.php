1c60490a6293735ea27091d7908a286610998705
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:36:27 -0300

    Create header.php

---
+++ app/views/shared/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title><?= isset($title) ? $title : 'SimpleVisitadores' ?></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+    </head>
+    <body>
