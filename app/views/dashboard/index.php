2903d94829feff754f63df61563cebac84556881
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:36:23 -0300

    Create index.php

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard - SimpleVisitadores</title>
+        <link rel="stylesheet" href="/assets/bulma.min.css">
+    </head>
+    <body>
+        <section class="section">
+            <div class="container">
+                <h1 class="title">Bem-vindo ao SimpleVisitadores</h1>
+            </div>
+        </section>
+    </body>
+</html>
