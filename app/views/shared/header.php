4fca906a3b5d186ed07e11ad23533b2138eea6b0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-30 12:38:01 -0300

    Update header.php

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -4,5 +4,6 @@
         <meta charset="UTF-8">
         <title><?= isset($title) ? $title : 'SimpleVisitadores' ?></title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+        <link rel="stylesheet" href="/assets/custom.css">
     </head>
     <body>
