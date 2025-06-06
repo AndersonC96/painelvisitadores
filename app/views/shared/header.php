9b7116a5d7a779ad2dc4084e35392fa7414788dc
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-06-06 10:24:29 -0300

    Update header.php

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -2,7 +2,8 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title><?= isset($title) ? $title : 'SimpleVisitadores' ?></title>
+        <title><?= isset($title) ? $title : 'Painel Visitadores' ?></title>
+        <link rel="icon" href="https://static.wixstatic.com/media/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
         <link rel="stylesheet" href="/assets/custom.css">
     </head>
