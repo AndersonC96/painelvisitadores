4e003e4bff097ac420519b7fe23d77aa117713d1
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-06-06 10:25:06 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,6 +17,30 @@
     $router->get('/usuarios/edit', 'UsuarioController@edit');
     $router->post('/usuarios/update', 'UsuarioController@update');
     $router->get('/usuarios/delete', 'UsuarioController@delete');
+    $router->get('/representantes', 'RepresentanteController@index');
+    $router->get('/representantes/create', 'RepresentanteController@create');
+    $router->post('/representantes/store', 'RepresentanteController@store');
+    $router->get('/representantes/edit', 'RepresentanteController@edit');
+    $router->post('/representantes/update', 'RepresentanteController@update');
+    $router->get('/representantes/delete', 'RepresentanteController@delete');
+    $router->get('/times', 'TimeController@index');
+    $router->get('/times/create', 'TimeController@create');
+    $router->post('/times/store', 'TimeController@store');
+    $router->get('/times/edit', 'TimeController@edit');
+    $router->post('/times/update', 'TimeController@update');
+    $router->get('/times/delete', 'TimeController@delete');
+    $router->get('/vendedoras', 'VendedoraController@index');
+    $router->get('/vendedoras/create', 'VendedoraController@create');
+    $router->post('/vendedoras/store', 'VendedoraController@store');
+    $router->get('/vendedoras/edit', 'VendedoraController@edit');
+    $router->post('/vendedoras/update', 'VendedoraController@update');
+    $router->get('/vendedoras/delete', 'VendedoraController@delete');
+    $router->get('/profissionais', 'ProfissionalController@index');
+    $router->get('/profissionais/create', 'ProfissionalController@create');
+    $router->post('/profissionais/store', 'ProfissionalController@store');
+    $router->get('/profissionais/edit', 'ProfissionalController@edit');
+    $router->post('/profissionais/update', 'ProfissionalController@update');
+    $router->get('/profissionais/delete', 'ProfissionalController@delete');
     // ---- Proteção das rotas internas ----
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = '/' . trim($uri, '/'); // Deixa no formato '/rota'
