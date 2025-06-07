12a5aaab9fdfd2c2f5c2a48eef2fa52751bdf448
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-06-07 16:46:13 -0300

    Update header.php

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -6,5 +6,6 @@
         <link rel="icon" href="https://static.wixstatic.com/media/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/5ede7b_719545c97a084f288b8566db52756425%7Emv2.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
         <link rel="stylesheet" href="/assets/custom.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
     </head>
     <body>
